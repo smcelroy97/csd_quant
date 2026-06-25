Corrected some issues in the ERP calculation with resampling

csd_erp.py
@@ -17,14 +17,15 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-data_dir = 'NKI_data/raw_files/long_soa/'
+data_dir = 'NKI_data/raw_files/'
+save_dir = 'NKI_data/csd_erps/'
 all_files = sorted([f for f in os.listdir(data_dir) if f.endswith(".mat")])
 files_per_rank = np.array_split(all_files, size)
 for file in files_per_rank[rank]:
     if '.mat' in file:
         fn = data_dir + file
         file_id = file[:-7]
-        samprds = 10000
+        samprds = 1000
 
         fp = h5py.File(fn, 'r')
 
@@ -64,7 +65,7 @@ for file in files_per_rank[rank]:
 
         fs = sampr
         tmin = 0.0  # 0 ms pre
-        tmax = 0.100   # 100 ms post
+        tmax = 0.2  # 100 ms post
 
         pre = int(round(abs(tmin) * fs))       # samples before trigger
         post = int(round(tmax * fs))            # samples after trigger
@@ -94,10 +95,10 @@ for file in files_per_rank[rank]:
         # E_good = E[good_epochs]
         #
         # print(f"Kept {E_good.shape[0]}/{E.shape[0]} epochs (ptp thr={thr:.4g})")
-'''
+
         erp_csd = E.mean(axis=0)
-        os.makedirs(f'{data_dir}csd_erps', exist_ok=True)
-        np.save(f'{data_dir}csd_erps/{file_id}_csd_erp', erp_csd)
+        os.makedirs(f'{save_dir}csd_erps', exist_ok=True)
+        np.save(f'{save_dir}csd_erps/{file_id}_csd_erp', erp_csd)
 
         #  plotting
         erp_csd_plot = erp_csd
@@ -132,6 +133,6 @@ for file in files_per_rank[rank]:
         ax.axvline(0, color='k', linestyle='--', linewidth=1)
 
         plt.tight_layout()
-        os.makedirs(f"{data_dir}csd_erps/plots", exist_ok=True)
-        plt.savefig(f"{data_dir}csd_erps/plots/{file_id}_csd_erp.jpg")
-'''
+        os.makedirs(f"{save_dir}csd_erps/plots", exist_ok=True)
+        plt.savefig(f"{save_dir}csd_erps/plots/{file_id}_csd_erp.jpg")
+

erp_pca.py
@@ -5,11 +5,11 @@ from mpi4py import MPI
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-data_dir = ('NKI_data/aligned_data_21/')
+data_dir = ('NKI_data/aligned_data/')
 all_files = sorted([f for f in os.listdir(data_dir) if f.endswith(".npy")])
 
-n_channels = 21
-n_time = 1000
+first = np.load(os.path.join(data_dir, all_files[0]))
+n_channels, n_time = first.shape
 
 flat_erps = []
 
@@ -22,38 +22,40 @@ for file in all_files:
 X = np.stack(flat_erps)
 
 pca = PCA(n_components=5)
-pca_erps = pca.fit_transform(flat_erps)
+pca_erps = pca.fit_transform(X)
 template = pca.components_[0].reshape(n_channels, n_time)
 
-np.save(f'{data_dir}/pc1_erp.npy', template)
+# np.save(f'{data_dir}/pc1_erp_redo.npy', template)
 
-time_ms = np.arange(template.shape[1])
+fs = 1000  # Hz
+time_ms = np.arange(template.shape[1]) / fs * 1000
 channels = np.arange(template.shape[0])
 
 v = np.percentile(np.abs(template), 99)
 levels = np.linspace(-v, v, 41)
 
 plot_tmin = 0
-plot_tmax = 50
+plot_tmax = 150
 
+mask = (time_ms >= plot_tmin) & (time_ms <= plot_tmax)
 
 fig, ax = plt.subplots(figsize=(6, 12))
-plt.xlim([0,50])
 
 cf = ax.contourf(
-    time_ms/10,
+    time_ms[mask],
     channels,
-    template,
+    template[:, mask],
     levels=levels,
     cmap='RdBu',
     extend='both'
 )
 
+ax.set_xlim(plot_tmin, plot_tmax)
 ax.invert_yaxis()
 ax.set_xlabel("Time (ms)")
 ax.set_ylabel("Channel")
-ax.set_title("Average CSD ERP")
+ax.set_title("30 channel aligned CSD ERP")
 ax.axvline(0, color='k', linestyle='--', linewidth=1)
 
 plt.tight_layout()
-plt.savefig(f"{data_dir}/plots/pc1_erp.jpg")
+plt.savefig(f"{data_dir}/plots/aligned30_erp_150ms.jpg")